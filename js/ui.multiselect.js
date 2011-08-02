528f43cbc7ad14794843198e8415c24e9970f3fa
Nate Eagleson <nate@ishness.net>
2011-08-02 09:55:02 +0800

    Stop selecting items via drag from causing duplicates.
    Previously, the class used to recognize the dropped element in the
    selected list's receive callback would process elements that were
    already selected, resulting in duplicate entries when these
    twice-processed entries were removed from the list of selected items.

--- js/ui.multiselect.js
+++ js/ui.multiselect.js
@@ -85,6 +85,12 @@ $.widget("ui.multiselect", {
 							$(this).data('optionLink').remove().appendTo(that.element);
 					});
 				},
+				beforeStop: function(event, ui) {
+					// This lets us recognize which item was just added to
+					// the list in receive, per the workaround for not being
+					// able to reference the new element.
+					ui.item.addClass('dropped');
+				},
 				receive: function(event, ui) {
 					ui.item.data('optionLink').attr('selected', true);
 					// increment count
@@ -92,8 +98,8 @@ $.widget("ui.multiselect", {
 					that._updateCount();
 					// workaround, because there's no way to reference
 					// the new element, see http://dev.jqueryui.com/ticket/4303
-					that.selectedList.children('.ui-draggable').each(function() {
-						$(this).removeClass('ui-draggable');
+					that.selectedList.children('.dropped').each(function() {
+						$(this).removeClass('dropped');
 						$(this).data('optionLink', ui.item.data('optionLink'));
 						$(this).data('idx', ui.item.data('idx'));
 						that._applyItemState($(this), true);
@@ -267,11 +273,11 @@ $.widget("ui.multiselect", {
 		});
 
 		// Double-clicks on an action link shouldn't do anything, as the
-		// single-click listener does all the work in this case. If we don't do
-		// this, then it is possible to create duplicates of an item by
-		// clicking on the action link, then clicking again as the next item
-		// slides into place beneath our cursor, triggering a double-click and
-		// a single click on our event listeners.
+		// single-click listener does all the work in this case.
+		// If we don't do this, then it is possible to create duplicates of an
+		// item by clicking on the action link, then clicking again as the next
+		// item slides into place beneath our cursor, triggering a double-click
+		// and a single click on our event listeners.
 		elements.find('a.action').dblclick(function(event) {
 			event.stopPropagation();
 		});
